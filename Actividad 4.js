839e18e7f339fd8dd84ba8c839b7a38d2c26e3b7
Jeison2110 <79480425+Jeison2110@users.noreply.github.com>
2023-12-16 12:28:17 -0500

    Add files via upload

---
+++ Actividad 4.js
@@ -0,0 +1,27 @@
+/*
+Creado por:
+    IdBanner:100098659
+    Nombre: Esperanza Castro Lombana 
+
+    IdBanner:100096167
+    Nombre: Jeison Valencia Sanchez
+
+    Fecha: 2023-12-16,  ©/ todos los derechos reservados
+    Corporación Universitaria Iberoamericana
+*/
+
+
+//Primer caso de Prueba//
+db.Arbitros.find().explain()
+db.Resultados.find({Fase: "All in ALL"}).explain()
+
+
+//Segundo caso de Prueba//
+db.Resultados.find({Fase: "All in ALL"}).pretty()
+db.Arbitros.find({Nombre}).pretty()
+db.Equipos.find().pretty()
+
+
+//Tercer caso de Prueba//
+db = (new Mongo("localhost:20006")).getDB("Torneo")
+show collections
